4bda93a1fb6264c095fbbc25b74faaf6f4cc11f0
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2024-06-17 15:30:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import { useState } from "react";
+// import Category from "./components/Category";
+// import Menu from "./components/menu";
+import data from "./data";
+
+function App() {
+  useState(data);
+ 
+  return (
+    <>
+      <h2>Grab Restaurant</h2>
+     {/* {
+    id: 1,
+    img: "https://food-cms.grab.com/compressed_webp/items/THITE2021081806195303702/detail/menueditor_item_fac456977b8e4c9988246ee248b7274d_1629267544022421163.webp",
+    name: "ลาบไก่ทอด",
+    Manoi: 1,
+    print: 169,
+  } */}
+    </>
+  );
+}
+export default App;
---
+++ src/data.js
@@ -0,0 +1,38 @@
+const menu = [
+  {
+    id: 1,
+    img: "https://food-cms.grab.com/compressed_webp/items/THITE2021081806195303702/detail/menueditor_item_fac456977b8e4c9988246ee248b7274d_1629267544022421163.webp",
+    name: "ลาบไก่ทอด",
+    Manoi: 1,
+    print: 169,
+  },
+  {
+    id: 2,
+    img: "https://food-cms.grab.com/compressed_webp/items/THITE2021081619591041232/detail/menueditor_item_ae89ea25d57e484ca21e0956a270f76e_1629143864089294634.webp",
+    name: "ปลากระพงทอดกระเทียมทั้งตัว",
+    Manoi: 2,
+    print: 350,
+  },
+  {
+    id: 3,
+    img: "https://food-cms.grab.com/compressed_webp/items/THITE2024032003542719954/detail/menueditor_item_f2344103269949e7af0b9f7e218e3fec_1710906839219009468.webp",
+    name: "หมูกรอบทอดพริกเกลือ",
+    Manoi: 3,
+    print: 159,
+  },
+  {
+    id: 4,
+    img: "https://food-cms.grab.com/compressed_webp/items/THITE2021081619510222303/detail/menueditor_item_2d71a8b99dec49b39a87431ae4acee9f_1629143430588796297.webp",
+    name: "กุ้งทอดกรอบราดซอสมะขาม",
+    Manoi: 4,
+    print: 250,
+  },
+  {
+    id: 5,
+    img: "https://food-cms.grab.com/compressed_webp/items/THITE2021081516093188852/detail/menueditor_item_1ecbb966745042cf9484aa378ccfb8ee_1629043709953179870.webp",
+    name: "ปลาสลิดทอดกรอบ",
+    Manoi: 5,
+    print: 89,
+  },
+];
+export default menu;
